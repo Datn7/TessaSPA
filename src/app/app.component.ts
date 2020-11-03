0d6421498f0462167724774040c335fd8aa1ce03
Datunashvili Giorgi <datunashvili_giorgi@yahoo.com>
2020-11-03 13:55:22 +0400

    firstCommunicationToHttp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination.model';
+import { IProduct } from './models/product.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'TessaSPA';
+  products: IProduct[];
+
+  constructor(private _http: HttpClient) {}
+
+  ngOnInit(): void {
+    this._http.get('http://localhost:5000/api/product').subscribe(
+      (res: IPagination) => {
+        this.products = res.data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
