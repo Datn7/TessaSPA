b965ea068a87e1ffb4327ca73c2d40aa75d41f10
Datunashvili Giorgi <datunashvili_giorgi@yahoo.com>
2020-11-04 22:06:02 +0400

    126Chatvlit

--- src/app/shop/product-details/product-details.component.ts
+++ src/app/shop/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IProduct } from 'src/app/shared/models/product.model';
+import { BreadcrumbService } from 'xng-breadcrumb';
 import { ShopService } from '../shop.service';
 
 @Component({
@@ -13,7 +14,8 @@ export class ProductDetailsComponent implements OnInit {
 
   constructor(
     private _shopService: ShopService,
-    private _activatedRoute: ActivatedRoute
+    private _activatedRoute: ActivatedRoute,
+    private _bcService: BreadcrumbService
   ) {}
 
   ngOnInit(): void {
@@ -26,6 +28,7 @@ export class ProductDetailsComponent implements OnInit {
       .subscribe(
         (product) => {
           this.product = product;
+          this._bcService.set('@productDetails', product.name);
         },
         (error) => {
           console.log(error);
